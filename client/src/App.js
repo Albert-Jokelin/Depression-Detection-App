ded71721372680919167bb1b4669ebb8dd5404af
Albert Jokelin <albertjokelin@icloud.com>
2022-08-04 19:55:02 +0530

    Added node modules

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,19 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
-import Auth4 from './components/Auth4'
+// import Auth4 from './components/Auth4'
 import './App.css'
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className='App'>
-        
+
         <div className='AppBody'>
           <Navbar />
           <Routes>
             <Route path='/' exact element={<Home />} />
-            <Route path='/auth' exact element={<Auth4 />} />
+            {/* <Route path='/auth' exact element={<Auth4 />} /> */}
           </Routes>
         </div>
       </div>
@@ -22,5 +22,5 @@ const App = () => {
   )
 }
 
-    
+
 export default App
