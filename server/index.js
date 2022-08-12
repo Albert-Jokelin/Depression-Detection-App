3eab15885b8cfb0c4dbedab1ddeaccdc0f25cdab
Albert Jokelin <albertjokelin@icloud.com>
2022-08-12 14:53:55 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use('/posts',postRoutes)
 
 // app.use('/user', userRouter)
 
-const CONNECTION_URL = "mongodb+srv://majorproject:<@Saritha954@majorprojectcluster.trma74d.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://majorproject:%40Saritha954@majorprojectcluster.trma74d.mongodb.net/?retryWrites=true&w=majority"
 
 const PORT = process.env.PORT || 5000
 
